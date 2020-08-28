a695572db58d67ee1a85c8aebb7ed3afa2030fd7
direyesm <37608963+direyesm@users.noreply.github.com>
2020-08-27 23:06:51 -0400

    Initial commit

---
+++ app/src/main/java/com/example/firstkotlin/Main.kt
@@ -0,0 +1,25 @@
+package com.example.firstkotlin
+
+fun main(){
+   // println("Hola Mundo")
+
+    //Declarando     Asignar  una variable
+    //var number  = 10
+    //val number2  = 20
+    //var name = "Diego"
+
+    //number = 30
+    //number = 25
+
+    val palabra01 = "Hola que tal"
+
+    var number = 10
+
+    //val concatenado = "El valor de la palabra 01 es : $number"
+    //val concatenado = "El valor de la palabra 01 es : ${palabra01.length}"
+    val concatenado = "El valor de la palabra 01 es : ${palabra01.length}"
+    println(concatenado)
+
+}
+
+
